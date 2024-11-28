5a81e71196016f503a321480556944919272b431
Sai Krishna <saikrishnabadineedi99@gmail.com>
2024-11-28 03:24:43 -0600

    final commit -27th-Sep

--- BEGIN BATCH.sql
+++ BEGIN BATCH.sql
@@ -62,3 +62,41 @@ BEGIN BATCH
   SET quantity =80
   WHERE item_id = a658becc-18fe-4246-be71-852b7113e5a3;
 APPLY BATCH;
+
+
+CREATE TABLE sensor_data (
+    sensor_id UUID,
+    timestamp TIMESTAMP,
+    parameter TEXT,
+    value DOUBLE,
+    PRIMARY KEY ((sensor_id, parameter), timestamp)
+) WITH CLUSTERING ORDER BY (timestamp DESC);
+
+insert into iot.sensor_data (sensor_id, timestamp, parameter, value) values (uuid(), toTimestamp(now()), 'temperature', 25.5);
+
+
+select * from iot.sensor_data;
+
+CREATE TABLE aggregated_data (
+    sensor_id UUID,
+    date DATE,
+    parameter TEXT,
+    avg_value DOUBLE,
+    max_value DOUBLE,
+    min_value DOUBLE,
+    PRIMARY KEY ((sensor_id, parameter), date)
+);
+
+select * from iot.aggregated_data;
+
+CREATE TABLE anomaly_detection (
+    sensor_id UUID,
+    timestamp TIMESTAMP,
+    parameter TEXT,
+    value DOUBLE,
+    anomaly_type TEXT,
+    PRIMARY KEY ((sensor_id, parameter), timestamp)
+) WITH CLUSTERING ORDER BY (timestamp DESC);
+
+
+select * from iot.anomaly_detection;
--- node_exporter_runtime.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start node_exporter with specified collectors and options
-/usr/local/bin/node_exporter \
-    --collector.disable-defaults \
-    --collector.textfile \
-    --collector.textfile.directory /tmp/node_exporter \
-    --web.listen-address=":9109"
